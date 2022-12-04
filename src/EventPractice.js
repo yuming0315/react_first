bc9ecfbf564c6cd8bd58df11ff876906d5ee3df3
yuming0315 <gjdbal111@naver.com>
2022-12-04 23:50:50 +0900

    4event

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,9 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
   render() {
     return (
       <div>
@@ -9,10 +12,26 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
           onChange={(e) => {
-            console.log(e);
+            this.setState(
+              {
+                message: e.target.value,
+              },
+              () => console.log(this.state.message)
+            );
           }}
         />
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({
+              message: "",
+            });
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
