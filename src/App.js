e4a0deb8f8506be7fce11bf383492f6596d7e116
yuming0315 <gjdbal111@naver.com>
2022-12-07 00:35:21 +0900

    5refDom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 //import MyComponent from "./MyComponent";
-import EventPractice from "./EventPractice";
-import "./App.css";
+import ScrollBox from "./ScrollBox";
+import { Component } from "react";
 
-const App = () => {
-  return <EventPractice />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />;
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/refSample.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class refSample extends Component {
+  input = React.createRef();
+
+  handleFocus = () => {
+    this.input.current.focus();
+  };
+
+  render() {
+    return (
+      <div>
+        <input ref={this.input} />
+      </div>
+    );
+  }
+}
