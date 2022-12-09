fbcfbe9dd097c2b6997ff55cf9fb8482e18a2f6e
yuming0315 <gjdbal111@naver.com>
2022-12-09 22:48:23 +0900

    6map

---
+++ src/IterationSample.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    setNames((prev) => [...prev, { id: nextId, text: inputText }]);
+    setInputText("");
+    setNextId((prev) => prev + 1);
+  };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
+};
+export default IterationSample;
